e220fc26e24897eab0b1646a416b2a3ed638a3f9
Julian Alves <julian@multilayer.io>
2023-04-04 15:53:47 +0200

    adding flag

--- gcsfuse/entrypoint.sh
+++ gcsfuse/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash          
 
 mkdir -p /content
-gcsfuse -o allow_other $GCS_BUCKET_NAME /content
+gcsfuse --implicit-dirs -o allow_other $GCS_BUCKET_NAME /content
 echo "Mounted succesfully FUSE bucket $GCS_BUCKET_NAME in /content"
 ls /content
